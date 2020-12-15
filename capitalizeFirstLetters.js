a1c46d081b841546572c5b05f8f92e00aa170d6c
FlavienBesseau <besseau.flavien@gmail.com>
2020-12-15 12:31:03 +0100

    git tag step-1

--- capitalizeFirstLetters.js
+++ capitalizeFirstLetters.js
@@ -14,8 +14,8 @@ assert.strictEqual(typeof capitalizeFirstLetters, "function");
 assert.strictEqual(capitalizeFirstLetters.length, 1);
 
 assert.strictEqual(
-  capitalizeFirstLetters("i love pizza with bacon"),
-  "I Love Pizza With Bacon"
+  capitalizeFirstLetters("i love pizzas with bacon"),
+  "I Love Pizzas With Bacon"
 );
 
 assert.strictEqual(capitalizeFirstLetters("zdsds"), "Zdsds");
